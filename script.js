48a9c9f7832fbec34d9ced4bc25eb9ad7c61a2c7
vincenzoimmordino92 <vincenzoimmordino92@gmail.com>
2023-10-31 12:03:56 +0100

    [add] addtask removetask

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ createApp({
     return{
       apiUrl:'server.php',
       titolo:'php-todo-list-json',
-      listTask: []
+      listTask: [],
+      newTask:'',
     }
   },
   methods:{
@@ -13,12 +14,32 @@ createApp({
       axios.get(this.apiUrl)
         .then(result =>{
           this.listTask = result.data;
-          console.log(this.listTask);
+          
         })
+    },
+    addNewTask(){
+      const dataForm = new FormData();
+      dataForm.append('newTaskItem', this.newTask);
+      dataForm.append('done', false);
 
+      axios.post(this.apiUrl,dataForm)
+        .then(result =>{
+          this.listTask = result.data;
+          this.newTask='';
+        })
+    },
+    removeTask(index){
+      const dataForm = new FormData();
+      dataForm.append('removeTask',index);
+      axios.post(this.apiUrl, dataForm)
+        .then(result => {
+          this.listTask= result.data;
+        })
     }
   },
+      
   mounted(){
     this.getList()
+  
   }
 }).mount('#app');
--- server.php
+++ server.php
@@ -6,11 +6,33 @@ $json_string = file_get_contents('./partials/todo-list.json');
 //trasformiamo la mia stringa in un array php
 $listTask = json_decode($json_string, true);
 
+
+
+
 // vari controlli 
+//controllo se i miei task sono arrivati e l'inserisco nel nostro array
+if(isset($_POST['newTaskItem']) && isset($_POST['done'])){
+
+  $listTask[] = [
+    "task" => $_POST['newTaskItem'],
+    "barred" => filter_var($_POST['done'],FILTER_VALIDATE_BOOLEAN)
+  ];
+}
+
+//controllo se la mia 
+if(isset($_POST['removeTask'])){
+  $indexRemoveTask = $_POST['removeTask'];
+  if($indexRemoveTask){
+    array_splice($listTask,$indexRemoveTask,1);
+  } 
+  
+} 
+
+
+file_put_contents('./partials/todo-list.json',json_encode($listTask));
 
 //trasformiamo la nostra listTask da file array php ad file jason array
 header('Content-Type: application/json');
-
 //ristampo la lista ricodificata da array ad stringa json
 echo json_encode($listTask);
 
